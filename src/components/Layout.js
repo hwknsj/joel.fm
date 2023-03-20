57af4db6dc59f6998331287d663012eb4e81a52c
joél hawkins torres <joel@joel.fm>
2023-03-19 17:27:09 -0700

    update resume

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -22,7 +22,7 @@ const Layout = ({ children }) => {
         }
       }
       resumeFile: file(
-        relativePath: { eq: "files/joel-hawkins-torres-resume-2022.pdf" }
+        relativePath: { eq: "files/joel-hawkins-torres-resume-2023.pdf" }
       ) {
         publicURL
       }
